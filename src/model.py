76e10d3b2873137433d2bc28fe0b33a2172a804b
wilhayla <wilhayla@hotmail.com>
2026-01-22 01:50:18 -0300

    archivo donde se definiran las funciones para entrenamiento

---
+++ src/model.py
@@ -0,0 +1,6 @@
+'''This file defines function like:
+- initialize_parameters(layers)
+- forward_propagation(X, parameters, dropout_rate)
+- backward_propagation(X, y, cache, parameters)
+- update_parameters(parameters, gradients, learning_rate)
+'''
