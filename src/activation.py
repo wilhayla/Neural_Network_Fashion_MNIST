17ad3ccee20f47414286a1b63c3b6659507e738e
wilhayla <wilhayla@hotmail.com>
2026-01-28 19:27:11 -0300

    agregando comentarios

--- src/activation.py
+++ src/activation.py
@@ -8,6 +8,7 @@ def relu(Z):
     """
     Función ReLU (Rectified Linear Unit).
     Si Z > 0, devuelve Z. Si Z <= 0, devuelve 0.
+    Esta funcion es la funcion ReLU que deja pasar la señal forward propagation.
     """
     return np.maximum(0, Z)
 
@@ -15,14 +16,24 @@ def relu_derivative(Z):
     """
     Derivada de ReLU. 
     Es 1 para valores positivos y 0 para negativos.
+    Esta funcion de ReLU deja pasar la señal en el back propagation.
     """
-    return Z > 0
+    return Z > 0 # Devuelve un valor booleano True/False que en matematica de python funciona como 1 o 0
+    # si Z > 0: la derivada es 1
+    # si Z < o igual a 0: la derivada es 0
 
 def softmax(Z):
     """
     Convierte las salidas de la última capa en probabilidades.
     Z es una matriz de (10, m).
+
+    Softmax devuelve un porcentaje de probabilidad, por ejemplo 10%, 55%, 99.1%, etc.
+    Por cada una de las predicciones se optiene su exponente natural con el numero de euler.
+    Los valores resultantes lo suma, y luego didive cada valor resultante entre esa suma.
+
+    La funcion softmax se utiliza en la capa de salida.
     """
+
     # Restamos el máximo de cada columna por estabilidad numérica 
     # (evita que np.exp explote con números grandes)
     exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
