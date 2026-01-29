f3b6627c9aa2ac6ae645bb80ad316696e810a385
wilhayla <wilhayla@hotmail.com>
2026-01-28 21:43:01 -0300

    Agregando comentarios

--- src/activation.py
+++ src/activation.py
@@ -43,12 +43,33 @@ def categorical_cross_entropy(y_true, y_pred):
     """
     y_true: Etiquetas reales en formato One-Hot (10, m)
     y_pred: Predicciones de la red tras Softmax (10, m)
+
+    El Trabajo de esta funcion es ponerle una nota (numero) a que tan bien o mal
+    lo hizo la red en sus predicciones.Si la nota es alta, la red se equivoco mucho, 
+    si es baja (cercana a cero), la red acerto.
+
+    y_true, y_pred, estan organizadas como matrices, donde las filas son las categorias (0 al 9),
+    y las columnas son los ejemplos individuales (imagenes)
     """
-    m = y_true.shape[1]
+    
+    m = y_true.shape[1] # m, es el numero total de imagenes que tenemos que procesar.
+
     # Usamos un pequeño epsilon (1e-15) para evitar el logaritmo de cero,
     # lo cual daría un error matemático.
-    epsilon = 1e-15
+    epsilon = 1e-15 # notacion cientifica (epsilon) es una notacion cientifica que representa
+                    # un valor casi cercano a cero (0.000000000000001)
+    ''' El valor de epsilon se crea para evitar que en la expresion mas abajo donde se utiliza 
+        la funcion para hallar el logaritmo de un numero no sea infinito. Esto es porque el logaritmo
+        de 0 no existe (tiende a infinito negativo). Esto evita que si la red neuronal asigna una 
+        probabilidad de exactamente 0.00000, al intentar calcular log(0), python arrojara un error
+        o un valor NaN (Not a Number). Esto romperia todo el entrenamiento.
+    '''
+    # linea de codigo que limita los valores minimos y maximos de y_pred
+    # el valor minimo de y_pred =  epsilon (0.000000000000001)
+    # el valor maximo de y_pred = 1 - epsilon (0.999999999999999)
     y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
     
+    # Esta linea es la formula matematica final que calcula el error total.
     loss = -np.sum(y_true * np.log(y_pred)) / m
+    
     return loss
